615e5771e435012b7b025767496136bc94fff45d
znznznn <78023622+znznznn@users.noreply.github.com>
2021-01-30 23:39:37 +0800

    Add files via upload

---
+++ 1_30/1_30/1_30_1.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int *arr = 1;
+//	printf("%d\n", *arr++);
+//	//printf("%d\n", *--arr);
+//}
+//void main(){//	int a[] = { 1, 7, 12, 15 };//	int *p1 = a, *p2 = p1++;//	*p1 += *p2++;//	printf("%d %d", *p1, *p2);//}
+//int main()
+//{
+//	/*int a = 3;
+//	int* p = &a;
+//	int */
+//	int a[] = { 1, 7, 12, 15 };//	printf("%p\n", a);
+//	printf("%p\n", a+1);
+//}
+//int main()
+//{
+//	printf("%d", printf("%d %d",43 ,34 ));
+//}
+//int main()
+//{
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+//	printf("%d\n", *(&arr));
+//	return 0;
+//}
