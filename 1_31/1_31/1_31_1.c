ed5055044eab785f5b89df11b9f0b56104fd6fb0
znznznn <78023622+znznznn@users.noreply.github.com>
2021-02-01 07:54:35 +0800

    Add files via upload

---
+++ 1_31/1_31/1_31_1.c
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 0 };
+//	//int* p = arr;
+//	char* p = arr;
+//	for (i = 0; i < 10;i++)
+//	{
+//		*(p + i) = 1;
+//	}
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int i = 0, a = 1, b = 2, c = 3, d = 4;
+//	//i = a++ && ++b && d++;
+//	// b+1 =3;
+//	// a && 3 = 1;
+//	// a+1 =2;
+//	// 1&&d = 1;
+//	// d = d+1 =5
+//	i = a++ || ++b || d++;
+//	printf("a = %d\n", a);
+//	printf("b = %d\n", b);
+//	printf("c = %d\n", c);
+//	printf("d = %d\n", d);
+//
+//	return 0;
+//}
+//求任何一个数中的二进制表达式中1的个数
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	int num = 0;
+//	scanf("%d", &num);
+//	for (i = 0; i < 32;i++)
+//	{
+//		if (((num>>1)&1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a^b;
+//	printf("%d\n", c);
+//	return 0;
+//}
